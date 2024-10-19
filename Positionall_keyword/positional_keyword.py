c33a7c2a393a75814499fab55a620322d5e18d7b
Ayush Tamang <ayutmg456@gmail.com>
2024-10-19 22:31:40 +0545

    positional argument vs keyword argument

--- Positionall_keyword/positional_keyword.py
+++ Positionall_keyword/positional_keyword.py
@@ -1,11 +1,23 @@
+# postional value(*)
+# keyword argument(**)
+
 def hello (name,age=21):
     print("Hello "+ name + ", you are " + str(age) + " years old.")
 
 
-hello("ATG",24)
+#hello("ATG",24)
 
 def hello(*args,**kwargs):
     print(args)
     print(kwargs)
 
-hello("ATG","learner",age=22,dob=1989)
+#hello("ATG","learner",age=22,dob=1989)
+
+lst=list(('ATG', 'learner'))
+dict_val={'age': 22, 'dob': 1989}
+
+hello(lst,dict_val)
+#considering both as a postional argument
+
+hello(*lst,**dict_val)
+#considering (first as postional argument,second as keyword argument)
---
+++ Positionall_keyword/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+hello(*lst,**dict_val)
